4866a654cded3e377538cf6dc4f3768c5e6db0a6
wenbin <wenbinlim93@gmail.com>
2024-08-31 07:44:04 +0800

    added styling using tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,46 @@
-import { useState } from "react"
+import { useState } from "react";
 
-import { getWeatherByLocationName } from "./api/data"
+import { getWeatherByLocationName } from "./api/data";
+
+import SearchIcon from "./icons/SearchIcon";
 
 function App() {
-  const [locationQuery, setLocationQuery] = useState("")
+  const [locationQuery, setLocationQuery] = useState("");
 
   const getWeather = async () => {
-    const res = await getWeatherByLocationName(locationQuery)
-    console.log(res)
-  }
+    const res = await getWeatherByLocationName(locationQuery);
+    console.log(res);
+  };
 
   return (
-    <div>
-      <header>
-        <input
-          type="text"
-          value={locationQuery}
-          onChange={(e) => setLocationQuery(e.target.value)}
-        />
-        <button onClick={getWeather}>Search</button>
+    <main className="h-screen bg-[url('./assets/bg-light.png')] p-4 dark:bg-[url('./assets/bg-dark.png')]">
+      <header className="mx-auto flex max-w-screen-sm gap-x-4">
+        <div className="bg-input dark:bg-input-dark flex w-full flex-col overflow-hidden rounded-xl px-6 py-2 dark:text-white">
+          <label
+            className="text-input-label dark:text-input-label-dark text-xs"
+            htmlFor="locationQuery"
+          >
+            Country
+          </label>
+          <input
+            className="bg-transparent focus:outline-none"
+            type="text"
+            name="locationQuery"
+            value={locationQuery}
+            onChange={(e) => setLocationQuery(e.target.value)}
+            autoFocus
+          />
+        </div>
+
+        <button
+          className="bg-primary dark:bg-primary-dark rounded-xl px-4 text-white"
+          onClick={getWeather}
+        >
+          <SearchIcon />
+        </button>
       </header>
-    </div>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#6C40B5",
+          dark: "#28114C"
+        },
+        input: {
+          DEFAULT: "rgba(255, 255, 255, 0.2)",
+          dark: "rgba(26, 26, 26, 0.5)"
+        },
+        "input-label": {
+          DEFAULT: "rgba(0, 0, 0, 0.4)",
+          dark: "rgba(255, 255, 255, 0.4)"
+        }
+      }
+    },
   },
   plugins: [],
 }
