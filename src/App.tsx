37a477a90e5f3f32fe649d2c2dcd33797d9fd654
wenbin <wenbinlim93@gmail.com>
2024-08-31 08:40:01 +0800

    result section

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,67 @@ import { useState } from "react";
 
 import { getWeatherByLocationName } from "./api/data";
 
+// assets
+import cloudImage from "./assets/cloud.png";
+import sunImage from "./assets/sun.png";
 import SearchIcon from "./icons/SearchIcon";
 
 function App() {
+  // query state
   const [locationQuery, setLocationQuery] = useState("");
 
+  // today weather state
+  const [todayWeatherDegree, setTodayWeatherDegree] = useState(0);
+  const [todayWeatherHigh, setTodayWeatherHigh] = useState(0);
+  const [todayWeatherLow, setTodayWeatherLow] = useState(0);
+  const [todayWeatherLocation, setTodayWeatherLocation] = useState("");
+  const [todayWeatherTime, setTodayWeatherTime] = useState("");
+  const [todayWeatherHumidity, setTodayWeatherHumidity] = useState(0);
+  const [todayWeatherDescription, setTodayWeatherDescription] = useState("");
+  const [todayWeatherImageAsSun, setTodayWeatherImageAsSun] = useState(false);
+
   const getWeather = async () => {
-    const res = await getWeatherByLocationName(locationQuery);
-    console.log(res);
+    try {
+      const res = await getWeatherByLocationName(locationQuery);
+
+      if (!res) {
+        console.log("no location found");
+        return;
+      }
+
+      console.log(res);
+
+      setTodayWeatherDegree(Math.round(res.main.temp));
+      setTodayWeatherHigh(Math.round(res.main.temp_max));
+      setTodayWeatherLow(Math.round(res.main.temp_min));
+      setTodayWeatherLocation(`${res.name}, ${res.sys.country}`);
+
+      // convert unix timestamp to date
+      const date = new Date(res.dt * 1000);
+      const dateString = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+
+      setTodayWeatherTime(dateString);
+
+      setTodayWeatherHumidity(res.main.humidity);
+      setTodayWeatherDescription(res.weather[0].main);
+
+      // https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
+      // weather code 800 is clear, using sun.png to represent clear weather
+      // other weather codes will be defaulted to cloud.png
+      if (res.weather[0].id === 800) {
+        setTodayWeatherImageAsSun(true);
+      } else {
+        setTodayWeatherImageAsSun(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <main className="h-screen bg-[url('./assets/bg-light.png')] p-4 dark:bg-[url('./assets/bg-dark.png')]">
       <header className="mx-auto flex max-w-screen-sm gap-x-4">
-        <div className="bg-input dark:bg-input-dark flex w-full flex-col overflow-hidden rounded-xl px-6 py-2 dark:text-white">
+        <div className="bg-input dark:bg-input-dark flex w-full flex-col overflow-hidden rounded-[20px] px-6 py-2 text-black dark:text-white">
           <label
             className="text-input-label dark:text-input-label-dark text-xs"
             htmlFor="locationQuery"
@@ -33,12 +80,39 @@ function App() {
         </div>
 
         <button
-          className="bg-primary dark:bg-primary-dark rounded-xl px-4 text-white"
+          className="bg-primary dark:bg-primary-dark rounded-[20px] px-4 text-white"
           onClick={getWeather}
         >
           <SearchIcon />
         </button>
       </header>
+
+      <section className="bg-result-section dark:bg-result-section-dark relative mx-auto mt-32 max-w-screen-sm rounded-[40px] border border-white/50 p-8 text-black dark:border-transparent dark:text-white">
+        <img
+          className="absolute -top-1/4 right-4 h-[150px] w-auto sm:-top-1/2 sm:right-0 sm:h-[300px]"
+          src={todayWeatherImageAsSun ? sunImage : cloudImage}
+          alt={todayWeatherImageAsSun ? "sun image" : "cloud image"}
+        />
+        <div className="flex justify-between">
+          <div>
+            <p>Today's Weather</p>
+            <p className="text-primary text-8xl font-bold dark:text-white">
+              {todayWeatherDegree}&#176;
+            </p>
+            <p>
+              H: {todayWeatherHigh}&#176; L: {todayWeatherLow}&#176;
+            </p>
+            <p className="font-bold text-[#666666] dark:text-white">
+              {todayWeatherLocation || "-"}
+            </p>
+          </div>
+          <div className="self-end text-right text-[#666666] sm:flex sm:flex-1 sm:flex-row-reverse sm:justify-between sm:gap-x-4 sm:text-center dark:text-white">
+            <p>{todayWeatherDescription || "-"}</p>
+            <p>Humidity: {todayWeatherHumidity}%</p>
+            <p>{todayWeatherTime || "-"}</p>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
--- src/api/data.ts
+++ src/api/data.ts
@@ -1,30 +1,30 @@
-import axios from "axios"
+import axios from "axios";
 
-const apiKey = import.meta.env.VITE_WEATHER_API_KEY as string
+const apiKey = import.meta.env.VITE_WEATHER_API_KEY as string;
 
 export const getWeatherByLocationName = async (locationName: string) => {
   try {
     const locationRes = await axios.get(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${locationName}&limit=1&appid=${apiKey}`
-    )
+      `http://api.openweathermap.org/geo/1.0/direct?q=${locationName}&limit=1&appid=${apiKey}`,
+    );
 
     // no location found
     if (locationRes.data.length === 0) {
-      console.log("no location found")
-      return null
+      console.log("no location found");
+      return null;
     }
 
     // get the first location, limit is set to 1
-    const { lat, lon } = locationRes.data[0]
+    const { lat, lon } = locationRes.data[0];
 
     const weatherRes = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
-    )
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`,
+    );
 
-    return weatherRes.data
+    return weatherRes.data;
   } catch (error) {
-    console.error(error)
+    console.error(error);
 
-    return null
+    return null;
   }
-}
+};
